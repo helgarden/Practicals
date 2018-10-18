c2d160b81c217f9470495d074c06aeece4e02b1f
Katherine Slutska <slutska.k@gmail.com>
2018-10-18 20:35:10 +0300

    added comments

--- Practical_1/script.js
+++ Practical_1/script.js
@@ -1,3 +1,4 @@
+//проверка, является ли введенное значение конечным числом больше нуля
 function check_input(text) {
   while (1) {
     var input = prompt(text);
@@ -5,14 +6,17 @@ function check_input(text) {
   }
 }
 
+//вычисление площади
 function calculate_square(a, b) {
   return (5*(a/3)*b);
 }
 
+//вывод на экран результатов
 function show_result(a, b) {
   alert("Площадь равна " + calculate_square(a, b));
 }
 
+//основной текст программы
 let a = check_input("Введите значение а:");
 let b = check_input("Введите значение b:");
 show_result(a, b);
