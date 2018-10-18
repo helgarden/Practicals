d7786c41e2e22274d0d25d352c938c9a1344ef39
Katherine Slutska <slutska.k@gmail.com>
2018-10-18 22:24:58 +0300

    minor improvements

--- Practical_2/script.js
+++ Practical_2/script.js
@@ -13,12 +13,8 @@ function check_triangle(a, b, c) {
 }
 
 function check_angle_type (angle) {
-    if (angle > (Math.PI / 2)) {
-      return "тупой";
-    }
-    if (angle < (Math.PI / 2)) {
-      return "острый";
-    }
+    if (angle > (Math.PI / 2)) return "тупой";
+    if (angle < (Math.PI / 2)) return "острый";
     return "прямой";
 }
 
@@ -26,11 +22,14 @@ var a, b, c;
 do {
   a = check_input("Введите значение а:");
   b = check_input("Введите значение b:");
-  c = check_input("Введите значение c:");}
+  c = check_input("Введите значение c:");
+}
 while (check_triangle (a, b, c));
+
 let bc = Math.acos((b*b+c*c-a*a)/(2*b*c));
 let ac = Math.acos((a*a+c*c-b*b)/(2*a*c));
 let ab = Math.PI - bc - ac;
+
 let result = "Угол между b и c " + check_angle_type(bc);
 result += "\nУгол между а и с " + check_angle_type(ac);
 result += "\nУгол между a и b " + check_angle_type(ab);
