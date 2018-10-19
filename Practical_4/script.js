450b6d27e15fb059d7b2fbe03c95d6f5c6183ea4
Katherine Slutska <slutska.k@gmail.com>
2018-10-19 17:06:20 +0300

    practical completed

---
+++ Practical_4/script.js
@@ -0,0 +1,16 @@
+const ACCURACY = 0.00001;
+const X = 0.56;
+let y_prev;
+let y = 0;
+let n = 1;
+let sum = 0;
+
+do  {
+  y_prev = y;
+  sum += 1 / (n * n);
+  y = 1 / (sum + Math.sin(X));
+  n++;
+}
+while (y - y_prev > ACCURACY);
+
+alert ("y = " + y);
