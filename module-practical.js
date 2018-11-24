2882956421f92e22d54334d661354a53d23da0c9
helgarden <slutska.k@gmail.com>
2018-11-24 14:13:45 +0200

    Changed code style

--- module-practical.js
+++ module-practical.js
@@ -1,25 +1,27 @@
-function check_input(text) {
+'use strict';
+
+function checkInput(text) {
   while (true) {
-    let input = prompt(text);
+    const input = prompt(text);
     if (isFinite(input) && (input > 0)) return input;
   }
 }
 
-function check_polindrom(input) {
+function checkPolindrom(input) {
   let number = [];
-  number = String(input).split("");
+  number = String(input).split('');
   for (let count = 0; count < (number.length / 2); count++) {
-    if (number[count] != number[number.length - 1 - count]) {
+    if (number[count] !== number[number.length - 1 - count]) {
       return false;
     }
   }
   return true;
 }
 
-let input = check_input("Введите значение разрядности");
+const input = checkInput('Введите значение разрядности');
 
 for (let counter = 0; counter < Math.pow(10, input); counter++) {
-  if (check_polindrom(counter)) {
+  if (checkPolindrom(counter)) {
     console.log(counter);
   }
 }
