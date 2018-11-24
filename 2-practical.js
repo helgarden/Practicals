c8f8a17437aa8b1166618874c0cfbca9cc701811
helgarden <slutska.k@gmail.com>
2018-11-24 14:32:53 +0200

    Changed code style

--- 2-practical.js
+++ 2-practical.js
@@ -1,37 +1,41 @@
-function check_input(text) {
+'use strict';
+
+function checkInput(text) {
   while (true) {
-    var input = prompt(text);
+    const input = prompt(text);
     if (isFinite(input) && (input > 0)) return input;
   }
 }
 
-function check_triangle(a, b, c) {
-  if ((Number(a) < (Number(b) + Number(c))) && (Number(b) < (Number(a) + Number(c))) && (Number(c) < (Number(a) + Number(b)))) {
+function checkTriangle(a, b, c) {
+  a = Number(a);
+  b = Number(b);
+  c = Number(c);
+  if ((a < b + c) && (b < a + c) && (c < a + b))
     return false;
-  }
   return true;
 }
 
-function check_angle_type (angle) {
-    if (angle > (Math.PI / 2)) return "тупой";
-    if (angle < (Math.PI / 2)) return "острый";
-    return "прямой";
+function checkAngleType(angle) {
+  if (angle > (Math.PI / 2)) return 'тупой';
+  if (angle < (Math.PI / 2)) return 'острый';
+  return 'прямой';
 }
 
-var a, b, c;
+let a, b, c;
 do {
-  a = check_input("Введите значение а:");
-  b = check_input("Введите значение b:");
-  c = check_input("Введите значение c:");
+  a = checkInput('Введите значение а:');
+  b = checkInput('Введите значение b:');
+  c = checkInput('Введите значение c:');
 }
-while (check_triangle (a, b, c));
+while (checkTriangle(a, b, c));
 
-let bc = Math.acos((b*b+c*c-a*a)/(2*b*c));
-let ac = Math.acos((a*a+c*c-b*b)/(2*a*c));
-let ab = Math.PI - bc - ac;
+const bc = Math.acos((b * b + c * c - a * a) / (2 * b * c));
+const ac = Math.acos((a * a + c * c - b * b) / (2 * a * c));
+const ab = Math.PI - bc - ac;
 
-let result = "a = " + a + ", b = " + b + ", c = " + c;
-result += "\nУгол между b и c " + check_angle_type(bc);
-result += "\nУгол между а и с " + check_angle_type(ac);
-result += "\nУгол между a и b " + check_angle_type(ab);
-alert(result);
+let result = 'a = ' + a + ', b = ' + b + ', c = ' + c;
+result += '\nУгол между b и c ' + checkAngleType(bc);
+result += '\nУгол между а и с ' + checkAngleType(ac);
+result += '\nУгол между a и b ' + checkAngleType(ab);
+console.log(result);
