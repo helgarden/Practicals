aeda2510b708c06c17b901ab0cd6e7f10c1540c5
helgarden <slutska.k@gmail.com>
2018-11-24 14:21:50 +0200

    Changed code style

--- 6-practical.js
+++ 6-practical.js
@@ -1,20 +1,23 @@
-function check_input(text) {
+'use strict';
+
+function checkInput(text) {
   while (true) {
-    var input = prompt(text);
+    const input = prompt(text);
     if (isFinite(input) && (input > 0)) return input;
   }
 }
 
-function fifth_root(x, iter_num){
-	var z = x;
-	for (let count = 1; count < iter_num; count++) {
-		z = (x / (z*z*z*z) + 4 * z) / 5;
-	}
-	return z;
+function root5(x, iterNum) {
+  let z = x;
+  for (let count = 1; count < iterNum; count++) {
+    z = (x / Math.pow(z, 4) + 4 * z) / 5;
+  }
+  return z;
 }
 
-let num_1 = check_input("Enter the first number: ");
-let num_2 = check_input("Enter the second number: ");
-let iter_num = check_input("Enter the number of iterations: ");
+const num1 = checkInput('Enter the first number: ');
+const num2 = checkInput('Enter the second number: ');
+const iterNum = checkInput('Enter the number of iterations: ');
+const result = root5(root5(num1 * num2, iterNum)) + root5(num1, iterNum);
 
-alert("Sum is " + (fifth_root(fifth_root(num_1 * num_2, iter_num)) + fifth_root(num_1, iter_num)));
+console.log('Sum is ' + result);
