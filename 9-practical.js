187c0a74fbc19d386156c0cd1120ff4d428fab29
helgarden <slutska.k@gmail.com>
2018-12-29 20:49:08 +0200

    Completed

--- 9-practical.js
+++ 9-practical.js
@@ -13,6 +13,7 @@ const reservedWords = [
   'else',
   'enum',
   'extern',
+  'false',
   'float',
   'for',
   'goto',
@@ -27,6 +28,7 @@ const reservedWords = [
   'static',
   'struct',
   'switch',
+  'true',
   'typedef',
   'union',
   'unsigned',
@@ -46,3 +48,42 @@ const reservedWords = [
   '_Static_assert',
   '_Thread_local'
 ];
+
+const stringArray = [
+  'int a = 0;',
+  'float b = a;',
+  'int c = while;',
+  'char d = 0afdd;',
+  'int e = Abcn;',
+  'boolean f = true;',
+  'int g = *3d);',
+  'float h = _hi;',
+  'a == m',
+];
+
+function isCorrectIdentificator(word) {
+  if (reservedWords.includes(word))
+    return false;
+  if (word[0] >= '0' && word[0] <= '9')
+    return false;
+  for (let i = 0; i < word.length; i++) {
+    if ((word[i] < 'A' || word[i] > 'z') &&
+        (word[i] < '0' || word[i] > '9') &&
+        word[i] !== '_')
+      return false;
+  }
+  return true;
+}
+
+function getIdentificators() {
+  stringArray.forEach((elem) => {
+    if (elem.includes('=')) {
+      const word = elem.substring(elem.indexOf('=') + 1, elem.indexOf(';')).trim();
+      if (isCorrectIdentificator(word)) {
+        console.log(word);
+      }
+    }
+  });
+}
+
+getIdentificators();
