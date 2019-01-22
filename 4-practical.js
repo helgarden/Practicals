b5163e8cb02af9e29ef40e573417094b7dfc83c2
helgarden <slutska.k@gmail.com>
2019-01-22 17:57:42 +0200

    Refactoring

--- 4-practical.js
+++ 4-practical.js
@@ -1,18 +1,21 @@
+/* Calculate y = (1 / sin(x)) * (1 / (1 / sum_from_1_to_infinity(n^2)))
+   for x = 0.56 with accuracy = 0.00001 */
+
 'use strict';
 
-const ACCURACY = 1e-5;
-const X = 0.56;
-let yPrev;
-let y = 0;
-let n = 1;
-let sum = 0;
+const calcY = (accuracy) => {
+  const sin = 1 / Math.sin(0.56);
+  let prev = 0;
+  let current = 0;
+  let n = 1;
+  let sum = 0;
+  do  {
+    prev = current;
+    sum += 1 / Math.pow(n++, 2);
+    current = sin / sum;
+  }
+  while (current - prev > accuracy);
+  return current;
+};
 
-do  {
-  yPrev = y;
-  sum += 1 / (n * n);
-  y = 1 / (sum + Math.sin(X));
-  n++;
-}
-while (y - yPrev > ACCURACY);
-
-console.log('y = ' + y);
+console.log('y = ' + calcY(0.00001));
