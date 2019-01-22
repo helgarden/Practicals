e36e3f650bf1fb27e90aed73c805aa95a8318150
helgarden <slutska.k@gmail.com>
2019-01-22 13:10:00 +0200

    Replace input with random value generation

--- 3-practical.js
+++ 3-practical.js
@@ -1,16 +1,16 @@
+/* Calculate the the nth member of the sequence given by the rule
+  a[0] = 1, (a[k] = k * a[k-1] + 1 / k) */
+
 'use strict';
 
-function checkInput(text) {
-  while (true) {
-    const input = prompt(text);
-    if (isFinite(input) && (input > 0)) return input;
-  }
-}
+const randomInt = (min, max) =>
+  Math.floor(min + Math.random() * (max - min + 1));
 
-const n = checkInput('Введите значение n:');
+const n = randomInt(1, 100);
 let a = 1;
 for (let k = a; k <= n; k++) {
   a = k * a + 1 / k;
 }
 
-console.log('n = ' + n + ', a = ' + a);
+console.log('n = ' + n);
+console.log('a = ' + a);
